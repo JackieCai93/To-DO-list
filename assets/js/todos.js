2a644790a31bf620b131d1f341bd3c9da5210747
JackieCai93 <46732969+JackieCai93@users.noreply.github.com>
2019-01-16 11:25:54 +0800

    Add files via Upload

---
+++ assets/js/todos.js
@@ -0,0 +1,28 @@
+// Check off Specific Todos by Clicking
+
+$("ul").on("click","li", function(){
+	$(this).toggleClass("completed");
+});
+
+//click on X to delete todos
+
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		var todoText = $(this).val();
+		$(this).val("");
+		//create a new li
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+	}
+
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+})
